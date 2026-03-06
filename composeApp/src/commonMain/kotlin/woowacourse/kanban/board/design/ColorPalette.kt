5e09c4d72dfccca857ff85acf0f13daff8e81e0d
nadajinny <jinsun23007@gmail.com>
2026-03-06 15:06:33 +0900

    refactor: tag 배경 색깔 상수화

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/design/ColorPalette.kt
@@ -0,0 +1,7 @@
+package woowacourse.kanban.board.design
+
+import androidx.compose.ui.graphics.Color
+
+object ColorPalette {
+    val LighterGray = Color(0xFFDEDEDE)
+}
--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/view/TagView.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/view/TagView.kt
@@ -6,8 +6,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import woowacourse.kanban.board.design.ColorPalette
 import woowacourse.kanban.board.model.Tag
 import woowacourse.kanban.board.design.FontSize
 
@@ -16,6 +16,6 @@ fun TagView(tag: Tag) {
     Text(
         text = tag.text,
         fontSize = FontSize.TAG.size,
-        modifier = Modifier.background(Color(0xFFDEDEDE), RoundedCornerShape(10.dp)).padding(4.dp),
+        modifier = Modifier.background(ColorPalette.LighterGray, RoundedCornerShape(10.dp)).padding(4.dp),
     )
 }
